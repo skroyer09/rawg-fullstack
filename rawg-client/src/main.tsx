c44357c9132ce7193753ecba8d395b0e191e1f80
steffen kroyer <stef5450@stud.kea.dk>
2024-11-04 09:13:50 +0100

    Update React DOM types to version 18.3.1 and add QueryClientProvider

--- rawg-client/src/main.tsx
+++ rawg-client/src/main.tsx
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme";
+import "./main.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
